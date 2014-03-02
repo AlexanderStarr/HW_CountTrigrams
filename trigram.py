bc47bbd199f6235cf48d66566350dac8db9b7e1c
Alexander Starr <AlexanderLStarr@gmail.com>
2014-03-02 09:18:50 -0600

    Added in support for finding trigrams across lines.

--- trigram.py
+++ trigram.py
@@ -7,18 +7,26 @@ class Trigram(MRJob):
         '''
         This mapper expects a text file.  It will count each trigram.
         '''
+        # Get the list of words from this line.
         currWords = line.split()
-        try:
-            prevLine = self.PrevLine
-        except:
-            prevLine = ''
-            self.PrevLine = prevLine
 
-        prevWords = prevLine.split()
-        #if len(prevWords) < 3:
+        # Find or create the list of the previous line's words.
+        try:
+            prevWords = self.PrevWords
+        except:
+            prevWords = []
+            self.PrevWords = prevWords
+
+        # We want to include the last 2 words from the previous line
+        # in our trigrams of this line, so attach them to our word list.
+        # Note: this works even if len(prevWords) < 2.
+        currWords = prevWords[-2:] + currWords
         for i in range(len(currWords) - 2):
             trigram = " ".join(currWords[i:i+3])
             yield (trigram, 1)
+
+        # Now save the currWords as the previous line's words.
+        self.PrevWords = currWords
 
     def reducer(self, key, values):
         yield (key, len(list(values)))
